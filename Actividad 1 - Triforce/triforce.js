73455a8536f689c659bd202071d2f2a241f465b4
albgv1402 <albertogarv1402@gmail.com>
2020-06-30 16:49:43 -0500

    ya se dibuja un triangulo

--- Actividad 1 - Triforce/triforce.js
+++ Actividad 1 - Triforce/triforce.js
@@ -1,12 +1,39 @@
+
 function sliderEvent() {
     document.getElementById("slider").oninput = function(event) {
         document.getElementById("sliderValue").innerHTML = "Value: " + event.target.value;
+        drawAll(event.target.value);
     };
+}
+
+function drawTriangle(ctx, x, y, side) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+
+    ctx.lineTo(x + side / 2, y - side);
+    ctx.lineTo(x + side, y);
+    ctx.lineTo(x, y);
+    
+    ctx.fill();
+    ctx.stroke();
+}
+
+function drawAll(cantDivisiones) {
+    let canvas = document.getElementById('htmlCanvas')
+    let canvasCtx = canvas.getContext("2d");
+    canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
+
+    let size = 520;
+
+
+    
+    for (let i = 0; i < cantDivisiones; i++) {
+        drawTriangle(canvasCtx, 100, 550 - i * 30, 520);
+    }
 }
 
 
 function main() {
     sliderEvent();
+    drawAll(0);
 }
-
-main();
