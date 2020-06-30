34ae9fa9cc1843d1feeeccd10f2b6e6e9b70b058
albgv1402 <albertogarv1402@gmail.com>
2020-06-30 17:45:43 -0500

    ya funciona

--- Actividad 1 - Triforce/triforce.js
+++ Actividad 1 - Triforce/triforce.js
@@ -2,11 +2,31 @@
 function sliderEvent() {
     document.getElementById("slider").oninput = function(event) {
         document.getElementById("sliderValue").innerHTML = "Value: " + event.target.value;
-        drawAll(event.target.value);
+        dibujarTodos(event.target.value);
     };
 }
 
-function drawTriangle(ctx, x, y, side) {
+// class Triangulo {
+//     constructor(x, y, length) {
+//         this.posX = x;
+//         this.posY = y;
+//         this.sideLenght = length;
+//     }
+
+//     dibujarTriangulo(ctx, x, y, side) {
+//         ctx.beginPath();
+//         ctx.moveTo(x, y);
+
+//         ctx.lineTo(x + side / 2, y - side);
+//         ctx.lineTo(x + side, y);
+//         ctx.lineTo(x, y);
+        
+//         ctx.fill();
+//         ctx.stroke();
+//     }
+// }
+
+function dibujarTriangulo(ctx, x, y, side) {
     ctx.beginPath();
     ctx.moveTo(x, y);
 
@@ -18,22 +38,30 @@ function drawTriangle(ctx, x, y, side) {
     ctx.stroke();
 }
 
-function drawAll(cantDivisiones) {
+function dividir(ctx, iteracion, x, y, tamaño) {
+
+    if (iteracion > 0) {
+        dividir(ctx, iteracion - 1, x, y, tamaño / 2);
+        dividir(ctx, iteracion - 1, x + tamaño / 2, y, tamaño / 2);
+        dividir(ctx, iteracion - 1, x + tamaño / 4, y - tamaño / 2, tamaño / 2);
+    } else {
+        dibujarTriangulo(ctx, x, y, tamaño);
+    }
+}
+
+function dibujarTodos(cantDivisiones) {
     let canvas = document.getElementById('htmlCanvas')
     let canvasCtx = canvas.getContext("2d");
     canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
 
     let size = 520;
+    let x = 100;
+    let y = 550;
 
-
-    
-    for (let i = 0; i < cantDivisiones; i++) {
-        drawTriangle(canvasCtx, 100, 550 - i * 30, 520);
-    }
+    dividir(canvasCtx, cantDivisiones, x, y, size);
 }
-
 
 function main() {
     sliderEvent();
-    drawAll(0);
+    dibujarTodos(0);
 }
